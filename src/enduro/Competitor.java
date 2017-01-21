34ab5ea2014910b75ea986b4756717c26e9e20bf
tobiaslandelius <tlandelius@gmail.com>
2017-01-21 21:45:44 +0100

    Added initial packages and empty files

---
+++ src/enduro/Competitor.java
@@ -0,0 +1,11 @@
+package enduro;
+
+public class Competitor<T> implements Comparable<T> {
+
+	@Override
+	public int compareTo(T arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/enduro/Enduro.java
@@ -0,0 +1,8 @@
+package enduro;
+
+public class Enduro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
